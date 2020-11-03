7b4fc05643e62917b0a8176e41226d04e9dfbc7f
Philipp Bartsch <pbartsch@team.mobile.de>
2020-11-03 15:05:54 +0100

    add missing license headers

--- src/test/java/org/example/move/IntegrationTestBase.java
+++ src/test/java/org/example/move/IntegrationTestBase.java
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------
+// Copyright 2020 mobile.de GmbH.
+// Author/Developer: Philipp Bartsch
+//
+// This code is licensed under MIT license (see LICENSE for details)
+//------------------------------------------------------------------
 package org.example.move;
 
 import org.junit.jupiter.api.extension.ExtendWith;
